852f3c304fa37f1566daefea3a7dc5cac4a4e812
Phuocbs <phuphuoc.pham111@gmail.com>
2026-04-19 21:39:59 +0700

    code_1

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 // import express from "express";
+import express from 'express'
+import { mapOrder } from './utils/sorts.js'
 
-const express = require("express");
-const app =express();
-const hostname ="localhost";
-const port     =8080;
+const app = express()
 
-app.get("/",(req,res) =>{
-    res.send('<h1> Hello')
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(mapOrder(
+    [ { id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' } ],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello hello World!</h1><hr>')
 })
-app.listen(port, hostname ,()=>{
-    console.log(`Hello local host:${hostname}`,`port:${port}`)
+
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello trello , I am running at ${ hostname }:${ port }/`)
 })
